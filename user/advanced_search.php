aeeed68507f6d63e60d0ccf131c6a9fe2e33406e
Ohkawa Sora <231079@st.yoshida-g.ac.jp>
2024-12-04 11:07:26 +0900

    検索

--- user/advanced_search.php
+++ user/advanced_search.php
@@ -3,13 +3,20 @@ include "../../db_open.php";
 include "../head.php";
 include "../header.php";
 echo "<link rel='stylesheet' href='header.css'>";
-echo "<link rel='stylesheet' href='./category.css'>";
+echo "<link rel='stylesheet' href='category.css'>";
+echo "<link rel='stylesheet' href='advance_search.css'>";
+
+$sql = "SELECT * FROM brand";
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+$brands = $stmt->fetchAll();
+
 ?>
 
 <form action="advanced_search.php" method="get">
     <!-- キーワード -->
     <label for="keyword" data-i18n="keyword_label">キーワード</label>
-    <input type="text" name="keyword" placeholder="キーワード" value="<?php echo isset($_GET['keyword']) ? htmlspecialchars($_GET['keyword']) : ''; ?>" data-i18n="keyword_placeholder"/>
+    <input type="text" name="keyword" placeholder="キーワード" value="<?php echo isset($_GET['keyword']) ? htmlspecialchars($_GET['keyword']) : ''; ?>" data-i18n="keyword_placeholder" />
 
     <!-- 性別 -->
     <label for="gender" data-i18n="gender_label">性別：</label>
@@ -17,6 +24,23 @@ echo "<link rel='stylesheet' href='./category.css'>";
         <option value="" data-i18n="all">すべて</option>
         <option value="1" <?php echo (isset($_GET['gender']) && $_GET['gender'] === '1') ? 'selected' : ''; ?> data-i18n="men">男性</option>
         <option value="0" <?php echo (isset($_GET['gender']) && $_GET['gender'] === '0') ? 'selected' : ''; ?> data-i18n="woman">女性</option>
+    </select>
+
+    <label for="brand" data-i18n="brand">ブランド：</label>
+    <select name="brand" id="brand">
+        <option value="">すべて</option> <!-- デフォルトで「すべて」選択肢を表示 -->
+        <?php
+        // $brands 配列を使ってブランドを表示
+        foreach ($brands as $brand) {
+            // ブランドIDと名前を表示
+            $brand_id = htmlspecialchars($brand['brand_id']);
+            $brand_name = htmlspecialchars($brand['brand_name']);
+
+            $selected = (isset($_GET['brand']) && $_GET['brand'] === $brand_id) ? 'selected' : '';
+
+            echo "<option value=\"$brand_id\" $selected>$brand_name</option>";
+        }
+        ?>
     </select>
 
     <!-- カテゴリー -->
@@ -40,9 +64,9 @@ echo "<link rel='stylesheet' href='./category.css'>";
 
     <!-- 価格帯 -->
     <label for="min_price" data-i18n="price_range">価格帯：</label>
-    <input type="number" name="min_price" placeholder="最小価格" value="<?php echo isset($_GET['min_price']) ? htmlspecialchars($_GET['min_price']) : ''; ?>" data-i18n="min_price_placeholder"/>
-    ～ 
-    <input type="number" name="max_price" placeholder="最大価格" value="<?php echo isset($_GET['max_price']) ? htmlspecialchars($_GET['max_price']) : ''; ?>" data-i18n="max_price_placeholder"/>
+    <input type="number" name="min_price" placeholder="最小価格" value="<?php echo isset($_GET['min_price']) ? htmlspecialchars($_GET['min_price']) : ''; ?>" data-i18n="min_price_placeholder" />
+    ～
+    <input type="number" name="max_price" placeholder="最大価格" value="<?php echo isset($_GET['max_price']) ? htmlspecialchars($_GET['max_price']) : ''; ?>" data-i18n="max_price_placeholder" />
 
     <!-- セール対象商品 -->
     <label for="sale_subject" data-i18n="sale_label">セール対象：</label>
@@ -52,104 +76,232 @@ echo "<link rel='stylesheet' href='./category.css'>";
         <option value="0" <?php echo (isset($_GET['sale_subject']) && $_GET['sale_subject'] === '0') ? 'selected' : ''; ?> data-i18n="no_sale">セールなし</option>
     </select>
 
-    <input type="submit" value="検索" data-i18n="search_button"/>
+    <input type="submit" value="検索" data-i18n="search_button" />
 </form>
 
 
 
 <script>
-// カテゴリーごとのサブカテゴリー
-const subcategories = {
-    1: [
-        { value: '1', text: 'Tシャツ/カットソー' },   // t-shirt, cutsew
-        { value: '2', text: 'シャツ/ブラウス' },     // shirt, blouse
-        { value: '3', text: 'ポロシャツ' },         // polo-shirt
-        { value: '4', text: 'ニット/セーター' },    // knit, sweater
-        { value: '5', text: 'ベスト' },            // vest
-        { value: '6', text: 'パーカー' }, // parka, sweat
-        { value: '7', text: 'スウェット' }, // sweat
-        { value: '8', text: 'カーディガン/ボレロ' }, // cardigan, bolero
-        { value: '9', text: 'アンサンブル' }, // ensemble
-        { value: '10', text: 'ジャージ' }, // jersey
-        { value: '11', text: 'タンクトップ' }, // tanktop
-        { value: '12', text: 'キャミソール' }, // camisole
-        { value: '13', text: 'チューブトップス' },   // tubetop
-        { value: '14', text: 'その他トップス' },    // other-tops
-    ],
-    2: [
-        { value: '15', text: 'ノーカラーコート' },    // collarless coat
-        { value: '16', text: 'ノーカラージャケット' },// collarless jacket
-        { value: '17', text: 'デニムジャケット' },    // denim jacket
-        { value: '18', text: 'ダウンジャケット' },    // down jacket
-        { value: '19', text: 'ダウンベスト' },        // down vest
-        { value: '20', text: 'ダッフルコート' },      // duffle coat
-        { value: '21', text: 'ブルゾン' },            // blouson
-        { value: '22', text: 'ミリタリージャケット' },// military jacket
-        { value: '23', text: 'モッズコート' },       // mods coat
-        { value: '24', text: 'ナイロンジャケット' }, // nylon jacket
-        { value: '25', text: 'ライダースジャケット' },// riders jacket
-        { value: '26', text: 'テーラードジャケット' }, // tailored jacket
-        { value: '27', text: 'トレンチコート' },      // trench coat
-        { value: '28', text: 'その他アウター' },      // other outerwear
-    ],
-    3: [
-        { value: '29', text: 'カーゴパンツ' },    // cargo pants
-        { value: '30', text: 'チノパン' },        // chino pants
-        { value: '31', text: 'デニムパンツ' },    // denim pants
-        { value: '32', text: 'スラックス' },      // slacks
-        { value: '33', text: 'スウェットパンツ' }, // sweat pants
-        { value: '34', text: 'その他パンツ' },    // other pants
-    ],
-    4: [
-        { value: '35', text: 'デニムスカート' },  // denim skirt
-        { value: '36', text: 'ミニスカート' },    // mini skirt
-        { value: '37', text: 'ミディスカート' },  // midi skirt
-        { value: '38', text: 'ロングスカート' },  // long skirt
-    ],
-    5: [
-        { value: '39', text: 'ドレス' },         //dress
-        { value: '40', text: 'ジャンパースカート' },  // jumper skirt
-        { value: '41', text: 'ワンピース' },            // onepiece dress
-        { value: '42', text: 'パンツドレス' },          // pants dress
-        { value: '43', text: 'シャツワンピース' },      // shirt dress
-        { value: '44', text: 'チュニック' },            // tunic
-    ],
-};
+    // カテゴリーごとのサブカテゴリー
+    const subcategories = {
+        1: [{
+                value: '1',
+                text: 'Tシャツ/カットソー'
+            }, // t-shirt, cutsew
+            {
+                value: '2',
+                text: 'シャツ/ブラウス'
+            }, // shirt, blouse
+            {
+                value: '3',
+                text: 'ポロシャツ'
+            }, // polo-shirt
+            {
+                value: '4',
+                text: 'ニット/セーター'
+            }, // knit, sweater
+            {
+                value: '5',
+                text: 'ベスト'
+            }, // vest
+            {
+                value: '6',
+                text: 'パーカー'
+            }, // parka, sweat
+            {
+                value: '7',
+                text: 'スウェット'
+            }, // sweat
+            {
+                value: '8',
+                text: 'カーディガン/ボレロ'
+            }, // cardigan, bolero
+            {
+                value: '9',
+                text: 'アンサンブル'
+            }, // ensemble
+            {
+                value: '10',
+                text: 'ジャージ'
+            }, // jersey
+            {
+                value: '11',
+                text: 'タンクトップ'
+            }, // tanktop
+            {
+                value: '12',
+                text: 'キャミソール'
+            }, // camisole
+            {
+                value: '13',
+                text: 'チューブトップス'
+            }, // tubetop
+            {
+                value: '14',
+                text: 'その他トップス'
+            }, // other-tops
+        ],
+        2: [{
+                value: '15',
+                text: 'ノーカラーコート'
+            }, // collarless coat
+            {
+                value: '16',
+                text: 'ノーカラージャケット'
+            }, // collarless jacket
+            {
+                value: '17',
+                text: 'デニムジャケット'
+            }, // denim jacket
+            {
+                value: '18',
+                text: 'ダウンジャケット'
+            }, // down jacket
+            {
+                value: '19',
+                text: 'ダウンベスト'
+            }, // down vest
+            {
+                value: '20',
+                text: 'ダッフルコート'
+            }, // duffle coat
+            {
+                value: '21',
+                text: 'ブルゾン'
+            }, // blouson
+            {
+                value: '22',
+                text: 'ミリタリージャケット'
+            }, // military jacket
+            {
+                value: '23',
+                text: 'モッズコート'
+            }, // mods coat
+            {
+                value: '24',
+                text: 'ナイロンジャケット'
+            }, // nylon jacket
+            {
+                value: '25',
+                text: 'ライダースジャケット'
+            }, // riders jacket
+            {
+                value: '26',
+                text: 'テーラードジャケット'
+            }, // tailored jacket
+            {
+                value: '27',
+                text: 'トレンチコート'
+            }, // trench coat
+            {
+                value: '28',
+                text: 'その他アウター'
+            }, // other outerwear
+        ],
+        3: [{
+                value: '29',
+                text: 'カーゴパンツ'
+            }, // cargo pants
+            {
+                value: '30',
+                text: 'チノパン'
+            }, // chino pants
+            {
+                value: '31',
+                text: 'デニムパンツ'
+            }, // denim pants
+            {
+                value: '32',
+                text: 'スラックス'
+            }, // slacks
+            {
+                value: '33',
+                text: 'スウェットパンツ'
+            }, // sweat pants
+            {
+                value: '34',
+                text: 'その他パンツ'
+            }, // other pants
+        ],
+        4: [{
+                value: '35',
+                text: 'デニムスカート'
+            }, // denim skirt
+            {
+                value: '36',
+                text: 'ミニスカート'
+            }, // mini skirt
+            {
+                value: '37',
+                text: 'ミディスカート'
+            }, // midi skirt
+            {
+                value: '38',
+                text: 'ロングスカート'
+            }, // long skirt
+        ],
+        5: [{
+                value: '39',
+                text: 'ドレス'
+            }, //dress
+            {
+                value: '40',
+                text: 'ジャンパースカート'
+            }, // jumper skirt
+            {
+                value: '41',
+                text: 'ワンピース'
+            }, // onepiece dress
+            {
+                value: '42',
+                text: 'パンツドレス'
+            }, // pants dress
+            {
+                value: '43',
+                text: 'シャツワンピース'
+            }, // shirt dress
+            {
+                value: '44',
+                text: 'チュニック'
+            }, // tunic
+        ],
+    };
 
-// カテゴリー変更時にサブカテゴリーを更新する関数
-function updateSubcategories() {
-    const categorySelect = document.getElementById('category');
-    const subcategoryContainer = document.getElementById('subcategory-container');
-    const subcategorySelect = document.getElementById('subcategory');
+    // カテゴリー変更時にサブカテゴリーを更新する関数
+    function updateSubcategories() {
+        const categorySelect = document.getElementById('category');
+        const subcategoryContainer = document.getElementById('subcategory-container');
+        const subcategorySelect = document.getElementById('subcategory');
 
-    // 選択されたカテゴリーの値を取得
-    const selectedCategory = categorySelect.value;
+        // 選択されたカテゴリーの値を取得
+        const selectedCategory = categorySelect.value;
 
-    // サブカテゴリーをクリア
-    subcategorySelect.innerHTML = '';
+        // サブカテゴリーをクリア
+        subcategorySelect.innerHTML = '';
 
-    // サブカテゴリーを非表示にする（デフォルト）
-    subcategoryContainer.style.display = 'none';
+        // サブカテゴリーを非表示にする（デフォルト）
+        subcategoryContainer.style.display = 'none';
 
-    // 選択されたカテゴリーに対応するサブカテゴリーを表示
-    if (subcategories[selectedCategory]) {
-        subcategories[selectedCategory].forEach((subcat) => {
-            const option = document.createElement('option');
-            option.value = subcat.value;
-            option.textContent = subcat.text;
-            subcategorySelect.appendChild(option);
-        });
+        // 選択されたカテゴリーに対応するサブカテゴリーを表示
+        if (subcategories[selectedCategory]) {
+            subcategories[selectedCategory].forEach((subcat) => {
+                const option = document.createElement('option');
+                option.value = subcat.value;
+                option.textContent = subcat.text;
+                subcategorySelect.appendChild(option);
+            });
 
-        // サブカテゴリーを表示
-        subcategoryContainer.style.display = 'block';
+            // サブカテゴリーを表示
+            subcategoryContainer.style.display = 'block';
+        }
     }
-}
 </script>
 
 <?php
 // フィルタリング条件を取得
 $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : null;
 $gender = isset($_GET['gender']) && $_GET['gender'] !== '' ? $_GET['gender'] : null;
+$brand = isset($_GET['brand_name']) && $_GET['brand_name'] !== '' ? $_GET['brand_name'] : null;
 $subcategory = isset($_GET['subcategory']) && $_GET['subcategory'] !== '' ? $_GET['subcategory'] : null;
 $category = isset($_GET['category']) && $_GET['category'] !== '' ? $_GET['category'] : null;
 $min_price = isset($_GET['min_price']) && $_GET['min_price'] !== '' ? $_GET['min_price'] : null;
@@ -177,6 +329,12 @@ if ($keyword !== null && $keyword !== '') {
 if ($gender !== null) {
     $sql .= " AND gender = ?";
     $params[] = $gender;
+}
+
+//ブランド
+if ($brand !== null) {
+    $sql .= " AND brand_name = ?";
+    $params[] = $brand;
 }
 
 // カテゴリーフィルターの追加
